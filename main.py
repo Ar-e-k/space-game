7a3d7aa2fdf9c66d3e91c9ae6d5d5c8a47dd76ab
A-r-ek <koxarek@gmail.com>
2021-02-08 21:27:43 +0100

    Masive changes

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 import pygame
 import sprites
-
-def main():
-    pygame.init()
-    screen=pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
-    playing=True #Determines is the game on or not
-    play=game(screen, sprites.player(10, 10, 10, 100))
-    paused=False
-
-    while playing: #Main gameloop
-        playing=play.frame()
-        for event in pygame.event.get():
-            if event.type==pygame.KEYDOWN:
-                if event.key==27: #key 27 is escape, clicking it quits the game
-                    play.end_game()
-                    playing=False
-                elif event.key==32: #pausing the game
-                    paused=True
-        while paused:
-            for event in pygame.event.get():
-                if event.type==pygame.KEYDOWN:
-                    if event.key==32:
-                        paused=False
-        pygame.display.flip()
-
+import math
+from random import randint, random
 
 class game:
 
@@ -32,23 +9,171 @@ class game:
         self._screen=screen
         self.player=player
 
-    def frame(self):
+        self._font=pygame.font.Font('freesansbold.ttf', 32) #the normal font used to display stuff
+
+        self._enemies=[sprites.enemy(100, 1, randint(0, screen_y-100), screen_x-100, [100, 100])]
+        self._enemy_probability=99.9
+
+        self.status=True
+
+    def update_acceleration(self, dir):
+        self.player.horixontal_thrust(dir[0])
+        self.player.vertical_thrust(dir[1])
+
+    def frame(self, dir, fire):
+        self.update_acceleration(dir)
+        self.player.update_velocity()
+        self.player.move()
+        self.spawn_enemis()
+        fire_check=self.check_fire(fire)
+        self.move_enemies()
         self.draw_background()
         self.draw_player()
-        return True
+        self.draw_enemy()
+        self.check_colisions()
+        if fire_check:
+            self.draw_fire()
+            self.check_colisions_laser()
+        return self.status
+
+    def spawn_enemis(self):
+        number=random()*100
+        if number>self._enemy_probability:
+            box1=sprites.enemy(100, 1, randint(0, screen_y-100), screen_x-100, [100, 100])
+            self._enemies.append(box1)
+        else:
+            self._enemy_probability=self._enemy_probability-0.0000001*self._enemy_probability
+
+
+    def move_enemies(self):
+        for enemy in self._enemies:
+            enemy.move()
+
+    def check_fire(self, fire):
+        if fire==True:
+            pass
+        else:
+            self.player.stop_fire()
+            if self.player.gun.check_fire():
+                return False
+            else:
+                self.player.gun.reload_timer()
+                return False
+        if self.player.fire():
+            return True
+        else:
+            self.player.stop_fire()
+            self.player.gun.reload_timer()
+            return False
 
     def draw_background(self):
-        rec=pygame.Rect([0, 0], [200, 100])
+        rec=pygame.Rect([0, 0], [screen_x, screen_y])
         pygame.draw.rect(self._screen, [255,255,255], rec)
 
+        ac=[
+            self.player.vertical_ac,
+            self.player.horizontal_ac
+            ]
+        self.get_text(str("vertical acceleration: "+str(round(ac[0], 5)*-1)), [0, 0])
+        self.get_text(str("horizontal acceleration: "+str(round(ac[1], 5))), [0, 50])
+
+        vel=[
+            self.player.vertical,
+            self.player.horizontal
+            ]
+        self.get_text(str("vertical velocity: "+str(round(vel[0], 5)*-1)), [600, 0])
+        self.get_text(str("horizontal velocity: "+str(round(vel[1], 5))), [600, 50])
+
+        amo, reload=self.player.gun.ammo, self.player.gun.reloadTime
+        self.get_text(str("ammo: "+str(amo)), [1100, 0])
+        self.get_text(str("reload: "+str(reload)), [1100, 50])
+
+    def get_text(self, text, pos):
+        acceleration=self._font.render(text, False, [0,0,0])
+        self._screen.blit(acceleration, pos)
+
     def draw_player(self):
-        position=self.player.return_cords()
-        self.box=pygame.Rect(position, [10, 10]) # The actuall box
-        pygame.draw.rect(self._screen, [0,0,0], self.box)
+        box=self.player.return_box()
+        pygame.draw.rect(self._screen, [0,0,0], box)
+
+    def draw_fire(self):
+        box=self.player.return_gun_box()
+        pygame.draw.rect(self._screen, [125,0,0], box)
+
+    def draw_enemy(self):
+        for enemy in self._enemies:
+            box=enemy.return_box()
+            pygame.draw.rect(self._screen, [0,0,0], box)
+
+    def check_colisions(self):
+        player_box=self.player.return_box()
+        for enemy in self._enemies:
+            box=enemy.return_box()
+            if player_box.colliderect(box):
+                self.end_game()
+
+    def check_colisions_laser(self):
+        laser_box=self.player.return_gun_box()
+        for index, enemy in enumerate(self._enemies):
+            box=enemy.return_box()
+            if box.colliderect(laser_box):
+                self._enemies.pop(index)
 
     def end_game(self):
-        return None
+        self.status=False
 
+def main():
+    pygame.init()
+    screen=pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    global screen_x, screen_y #globalises the hight and the width of the screen
+    screen_x, screen_y=pygame.display.Info().current_w, pygame.display.Info().current_h
+
+    playing=True #Determines is the game on or not
+    player= sprites.player(100, 100, 100000, 100, [200, 100], [screen_x, screen_y])
+    player.load_gun(sprites.gun(100, 1, [screen_x, 5], 400))
+    play=game(screen, player)
+    paused=False
+
+    direction=[0, 0]
+    fire=False
+
+    while playing: #Main gameloop
+        playing=play.frame(direction, fire)
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN:
+                if event.key==27: #key 27 is escape, clicking it quits the game
+                    play.end_game()
+                    playing=False
+                elif event.key==32: #pausing the game
+                    paused=True
+                elif event.key in [97, 100]:
+                    val=int(event.key)
+                    val=val-97
+                    val=val/1.5
+                    val-=1
+                    direction[0]=val
+                elif event.key in [119, 115]:
+                    val=int(event.key)
+                    val=val-115
+                    val=val/2
+                    val-=1
+                    val*=-1
+                    direction[1]=val
+                elif event.key==102:
+                    fire=True
+            if event.type==pygame.KEYUP:
+                if event.key in [97, 100]:
+                    direction[0]=0
+                elif event.key in [119, 115]:
+                    direction[1]=0
+                elif event.key==102:
+                    fire=False
+        while paused:
+            for event in pygame.event.get():
+                if event.type==pygame.KEYDOWN:
+                    if event.key==32:
+                        paused=False
+        pygame.display.flip()
 
 if __name__=="__main__":
     main()
--- sprites.py
+++ sprites.py
@@ -1,6 +1,12 @@
+from copy import copy
+from time import sleep
+import pygame
+
 class player:
 
-    def __init__(self, x, y, mass, thrust):
+    def __init__(self, x, y, mass, thrust, size, map_size):
+        self.map_size=map_size
+
         self.x_cord=x
         self.y_cord=y
 
@@ -12,21 +18,115 @@ class player:
         self.horizontal_ac=0
 
         self.acceleration_mod=thrust/mass
+        self.size=size
+
+        self.recoil=0
+
+    def load_gun(self, gun):
+        self.gun=gun
 
     def move(self):
         self.x_cord+=(self.horizontal*self.modifier)
         self.y_cord+=(self.vertical*self.modifier)
+        self.teleport()
 
     def update_velocity(self):
         self.vertical+=self.vertical_ac
-        self.horizontal+=self.horizontal_ac
-
+        self.horizontal+=self.horizontal_ac-self.recoil
 
     def vertical_thrust(self, direction=1):
-        self.vertical_ac+=self.acceleration_mod*direction
+        self.vertical_ac=self.acceleration_mod*direction
 
     def horixontal_thrust(self, direction=1):
-        self.horizontal_ac+=self.acceleration_mod*direction
+        self.horizontal_ac=self.acceleration_mod*direction
 
-    def return_cords(self):
-        return [self.x_cord, self.y_cord]
+    def fire(self):
+        if self.gun.check_fire():
+            self.recoil=0.005
+            self.gun.fire()
+            return True
+        return None
+
+    def stop_fire(self):
+        self.recoil=0
+
+    def return_box(self):
+        box=pygame.Rect([self.x_cord, self.y_cord], self.size) # The actuall box
+        return box
+
+    def return_gun_box(self):
+        return self.gun.return_box([self.x_cord, self.y_cord], self.size)
+
+    def teleport(self):
+        if self.x_cord>self.map_size[0]-self.size[0]:
+            self.x_cord=self.map_size[0]-self.size[0]
+        if self.x_cord<0:
+            self.x_cord=0
+        if self.y_cord>self.map_size[1]-self.size[1]:
+            self.y_cord=self.map_size[1]-self.size[1]
+        if self.y_cord<0:
+            self.y_cord=0
+        '''while self.x_cord>self.map_size[0]:
+            self.x_cord-=self.map_size[0]
+        while self.x_cord<0:
+            self.x_cord+=self.map_size[0]
+        while self.y_cord>self.map_size[1]:
+            self.y_cord-=self.map_size[1]
+        while self.y_cord<0:
+            self.y_cord+=self.map_size[1]'''
+
+
+class gun:
+
+    def __init__(self, ammo, damage, size, reload):
+        self.damage=damage
+
+        self.max_ammo=ammo
+        self.ammo=copy(ammo)
+
+        self.reloadTime=0
+        self.max_reload=reload
+
+        self.laser_size=size
+
+    def reload(self):
+        self.reloadTime=copy(self.max_reload)
+
+    def reload_timer(self):
+        if self.ammo<self.max_ammo:
+            self.ammo+=int(self.max_ammo/self.reloadTime)
+        else:
+            self.ammo=copy(self.max_ammo)
+        self.reloadTime-=1
+
+    def check_fire(self):
+        if self.reloadTime==0:
+            return True
+        else:
+            return False
+
+    def fire(self):
+        self.ammo-=1
+        if self.ammo==0:
+            self.reload()
+
+    def return_box(self, position, size):
+        position[0]+=(size[0]/2)
+        position[1]+=(size[1]/2)
+        box=pygame.Rect(position, self.laser_size)
+        return box
+
+class enemy:
+
+    def __init__(self, health, speed, y_cord, x_cord, size):
+        self.health=health
+        self.speed=speed
+        self.cords=[x_cord, y_cord]
+        self.size=size
+
+    def move(self):
+        self.cords[0]-=self.speed
+
+    def return_box(self):
+        box=pygame.Rect(self.cords, self.size) # The actuall box
+        return box
